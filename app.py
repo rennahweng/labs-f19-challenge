3babee26b6598b2bf8be63db60673c0be9cb4c0a
rennahweng <rennahweng628@gmail.com>
2019-09-24 01:31:52 -0400

    ADI Labs Challenge Task 3 is Completed, Display Pokemon ID and Name using PokeApi.

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask
+import requests 
 
 app = Flask(__name__)
 
+@app.route("/pokemon/<query>")
+def pokemon(query):
+	api_url = "https://pokeapi.co/api/v2/pokemon/" + 
+	r = requests.get(api_url)
 
-@app.route('/', methods=['GET'])
-def main():
-    return render_template('index.html')
+    # if the query entered is an id integer
+	if(query.isdigit()):
+        # displace the pokemon name with its id
+		return 'The pokemon with id %s is %s' % (query,r.json()["name"])
 
+    # else if the query entered is a pokemon name string
+	else:
+        # displace the pokemon id with its name
+		return '%s has id %s' % (query,r.json()["id"])
+	
 
 if __name__ == '__main__':
-    app.run()
+   app.run(debug=True)
